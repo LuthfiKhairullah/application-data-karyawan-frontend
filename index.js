adbe0ae0fedf6ce0df9557ac48288d71a475c7db
LuthfiKhairullah <5luthfi.khairullah@gmail.com>
2024-07-21 09:11:11 +0700

    update fix bug select2

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
   });
-  // win.webContents.openDevTools();
   win.loadFile(path.join(__dirname, 'index.html'));
 }
 
